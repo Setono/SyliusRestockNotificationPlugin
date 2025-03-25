4e4e96806f62bca0984c9c39c92022847bbfa686
Joachim Løvgaard <joachim@loevgaard.dk>
2025-03-25 15:43:13 +0100

    Remove dependency on variant link plugin

--- src/SetonoSyliusRestockNotificationPlugin.php
+++ src/SetonoSyliusRestockNotificationPlugin.php
@@ -4,22 +4,13 @@ declare(strict_types=1);
 
 namespace Setono\SyliusRestockNotificationPlugin;
 
-use Setono\SyliusRestockNotificationPlugin\DependencyInjection\Compiler\RegisterUrlGeneratorPass;
 use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
 use Sylius\Bundle\ResourceBundle\AbstractResourceBundle;
 use Sylius\Bundle\ResourceBundle\SyliusResourceBundle;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 final class SetonoSyliusRestockNotificationPlugin extends AbstractResourceBundle
 {
     use SyliusPluginTrait;
-
-    public function build(ContainerBuilder $container): void
-    {
-        parent::build($container);
-
-        $container->addCompilerPass(new RegisterUrlGeneratorPass());
-    }
 
     public function getSupportedDrivers(): array
     {
