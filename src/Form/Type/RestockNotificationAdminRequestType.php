7df1c2aa19f0e0ac04ac8ec54cd171401ea55145
Joachim Løvgaard <joachim@loevgaard.dk>
2025-05-20 10:59:56 +0200

    We don't need a parent

--- src/Form/Type/RestockNotificationAdminRequestType.php
+++ src/Form/Type/RestockNotificationAdminRequestType.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace Setono\SyliusRestockNotificationPlugin\Form\Type;
 
+use Sylius\Bundle\ChannelBundle\Form\Type\ChannelChoiceType;
+use Sylius\Bundle\LocaleBundle\Form\Type\LocaleChoiceType;
+use Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 
-final class RestockNotificationAdminRequestType extends RestockNotificationRequestType
+final class RestockNotificationAdminRequestType extends AbstractResourceType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
@@ -15,6 +19,16 @@ final class RestockNotificationAdminRequestType extends RestockNotificationReque
         $builder
             ->add('productVariant', ProductVariantAutocompleteChoiceType::class, [
                 'label' => 'sylius.ui.variant',
+            ])
+            ->add('channel', ChannelChoiceType::class, [
+                'placeholder' => 'setono_sylius_restock_notification.form.restock_notification_request.channel_placeholder',
+                'label' => 'sylius.ui.channel',
+            ])
+            ->add('localeCode', LocaleChoiceType::class, [
+                'label' => 'sylius.ui.locale',
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'sylius.ui.email',
             ])
         ;
     }
